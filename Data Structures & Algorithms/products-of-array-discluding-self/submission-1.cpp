3966ca59a942eba34fec8617214014129031bdbd
ShubhJ46 <87970238+ShubhJ46@users.noreply.github.com>
2026-05-04 22:36:05 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int product = 1, zeroes = 0;
+        for (int i = 0; i< nums.size(); i++) {
+            if (nums[i]) {
+                product *= nums[i];
+            } else {
+                zeroes++;
+            }
+
+        }
+
+        // cout << "Product: " << product << " zeroes: " << zeroes << endl;
+
+        vector<int> res;
+        for(int i = 0; i<nums.size(); i++) {
+            if (zeroes) {
+                if (zeroes > 1 || nums[i]) {
+                    res.push_back(0);
+                } else {
+                    res.push_back(product);
+                }
+            } else {
+                res.push_back(product/nums[i]);
+            }
+        }
+
+        return res;
+
+    }
+};
